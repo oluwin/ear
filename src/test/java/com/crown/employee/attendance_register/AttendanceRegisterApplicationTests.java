791cdfa8db003c2b079d6577af6ed9518ceb9d0d
Adebumiti Olusegun A. <39396698+oluwin@users.noreply.github.com>
2025-03-01 22:12:19 +0100

    initial commit

---
+++ src/test/java/com/crown/employee/attendance_register/AttendanceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crown.employee.attendance_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
