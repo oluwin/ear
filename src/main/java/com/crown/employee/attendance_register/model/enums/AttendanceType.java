de03b50eec240e1298165f1fb621e374b6d9774f
Adebumiti Olusegun A. <39396698+oluwin@users.noreply.github.com>
2025-03-02 07:34:50 +0100

    Dockerized the service

--- src/main/java/com/crown/employee/attendance_register/model/enums/AttendanceType.java
+++ src/main/java/com/crown/employee/attendance_register/model/enums/AttendanceType.java
@@ -3,5 +3,6 @@ package com.crown.employee.attendance_register.model.enums;
 public enum AttendanceType {
     PRESENT,       // Employee was present and signed in/out
     SICK_LEAVE,    // Employee was on sick leave
-    ABSENCE        // Employee was absent without leave
+    ABSENCE,       // Employee was absent with permission
+    AWOL           // Employee was absent without official leave
 }
