49b0fbf1b623f87174a24f2358a4840ad24c1271
Adebumiti Olusegun A. <39396698+oluwin@users.noreply.github.com>
2025-03-02 20:57:51 +0100

    Added DTOs

---
+++ src/main/java/com/crown/employee/attendance_register/data/AttendanceResponseDto.java
@@ -0,0 +1,72 @@
+package com.crown.employee.attendance_register.data;
+
+import com.crown.employee.attendance_register.model.enums.AttendanceType;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AttendanceResponseDto {
+    private Long id;
+    private Long employeeId;
+    private String employeeName;
+    private LocalDate date;
+    private LocalTime signInTime;
+    private LocalTime signOutTime;
+    private AttendanceType attendanceType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getSignInTime() {
+        return signInTime;
+    }
+
+    public void setSignInTime(LocalTime signInTime) {
+        this.signInTime = signInTime;
+    }
+
+    public LocalTime getSignOutTime() {
+        return signOutTime;
+    }
+
+    public void setSignOutTime(LocalTime signOutTime) {
+        this.signOutTime = signOutTime;
+    }
+
+    public AttendanceType getAttendanceType() {
+        return attendanceType;
+    }
+
+    public void setAttendanceType(AttendanceType attendanceType) {
+        this.attendanceType = attendanceType;
+    }
+}
